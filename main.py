5b0a79e841eac48ca591c2f64738871cccccf7f6
vnnsaybot <vnnsaybot@gmail.com>
2026-05-07 23:30:09 +1000

    + main.py (HOST)

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='static/templates')
+app.config['SECRET_KEY'] = 'hardhardhard'
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
